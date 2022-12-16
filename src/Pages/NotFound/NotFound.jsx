2a1175c2d19e7b196b38b8d8d879ad9ed3f13087
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2022-12-16 21:42:06 +0400

    Update NotFound.jsx

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-      <h1 style={{textAlign:"center"}}>Bu koda uyqun melimat tapilmadi</h1>
+      <h1 style={{textAlign:"center"}}>Təəsüfki daxil etdiyiniz məlumata uyğun nəticə tapılmadı,zəhmət olmasa kodun düz olduğundan əmin olub yenidən daxil edin</h1>
     </div>
   )
 }
