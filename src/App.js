34b9f98b8fbacb9fce625afff549fca9a92884a0
Shikhkarim <sixkerimmemmedov2001@gmail.com>
2022-12-16 19:19:21 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import users from './elnare.json';
+import Home from './Pages/Home/Home';
+import Result from './Pages/Result/Result'
+import NotFound from './Pages/NotFound/NotFound'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+}
+from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='mark/:id' element={<Result result={users} />} />
+          <Route path='notfound' element={<NotFound />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>Bu koda uyqun melimat tapilmadi</h1>
+    </div>
+  )
+}
