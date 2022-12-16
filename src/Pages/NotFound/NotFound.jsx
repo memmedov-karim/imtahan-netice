d79b1ed96f78b9fe7507719e4f0a42937fa4ba73
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2022-12-16 21:43:47 +0400

    Update NotFound.jsx

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 export default function NotFound() {
   return (
     <div>
-      <h1 style={{textAlign:"center"}}>Təəsüfki daxil etdiyiniz məlumata uyğun nəticə tapılmadı,zəhmət olmasa kodun düz olduğundan əmin olub yenidən daxil edin</h1>
+      <h1 style={{textAlign:"center"}}>Təəsüfki daxil etdiyiniz məlumata uyğun nəticə tapılmadı,zəhmət olmasa kodun düz olduğundan əmin olub yenidən <Link to="/">Yenidən daxil edin</Link></h1>
     </div>
   )
 }
